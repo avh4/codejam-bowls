e18d87d5e9d7f6bee2414ded4062bff17f0a3272
Aaron VonderHaar <Aaron.VonderHaar@flextronics.com>
2010-03-19 16:46:11 -0700

    Refactor to use maps

--- src/BowlProblemTest.java
+++ src/BowlProblemTest.java
@@ -15,8 +15,7 @@ public class BowlProblemTest {
     @Test
     public void simplestMixture() {
         String input = "1\n" // 1 test case
-                + "1\n" // 1 recipe
-                + "SANDWICH 2 peanuts fruit\n";
+                + recipes(1) + "SANDWICH 2 peanuts fruit\n";
 
         String output = "Case #1: 1\n";
         assertEquals(output, mBowlSolver.solve(input));
@@ -25,8 +24,7 @@ public class BowlProblemTest {
     @Test
     public void mixtureWithSubmixture() {
         String input = "1\n" // 1 test case
-                + "2\n" // 2 recipes
-                + "SANDWICH 2 meat FILLING\n" + "FILLING 2 bread cheese\n";
+                + recipes(2) + "SANDWICH 2 meat FILLING\n" + "FILLING 2 bread cheese\n";
 
         String output = "Case #1: 2\n";
         assertEquals(output, mBowlSolver.solve(input));
@@ -35,10 +33,9 @@ public class BowlProblemTest {
     @Test
     public void submixtureWithTwoSubsubs() {
         String input = "1\n" // 1 test case
-                + "4\n" // 2 recipes
-                + "SANDWICH 2 meat FILLING\n" + "FILLING 2 BREAD CHEESE\n"
-                + "BREAD 2 wheat salt\n"
-                + "CHEESE 2 milk mold\n";
+                + recipes(4) + "SANDWICH 2 meat FILLING\n"
+                + "FILLING 2 BREAD CHEESE\n"
+                + "BREAD 2 wheat salt\n" + "CHEESE 2 milk mold\n";
 
         String output = "Case #1: 3\n";
         assertEquals(output, mBowlSolver.solve(input));
@@ -47,8 +44,7 @@ public class BowlProblemTest {
     @Test
     public void googleSample1() {
         String input = "1\n" // 1 test case
-                + "3\n" // 3 recipes
-                + "SOUP 3 STOCK salt water\n"
+                + recipes(3) + "SOUP 3 STOCK salt water\n"
                 + "STOCK 2 chicken VEGETABLES\n"
                 + "VEGETABLES 2 celery onions\n";
 
@@ -59,14 +55,52 @@ public class BowlProblemTest {
     @Test
     public void googleSample2() {
         String input = "1\n" // 1 test case
-                + "5\n" // 5 recipes
+                + recipes(5)
                 + "MILKSHAKE 4 milk icecream FLAVOR FRUIT\n"
                 + "FRUIT 2 banana berries\n"
                 + "FLAVOR 2 SPICES CHOCOLATE\n"
-                + "SPICES 2 nutmeg cinnamon\n"
-                + "CHOCOLATE 2 cocoa syrup\n";
+                + "SPICES 2 nutmeg cinnamon\n" + "CHOCOLATE 2 cocoa syrup\n";
 
         String output = "Case #1: 3\n";
         assertEquals(output, mBowlSolver.solve(input));
+    }
+
+    @Test
+    public void constrainedByIngredientMixingPre() {
+        String input = "1\n" // comment
+                + recipes(4) //
+                + "AA 3 AAA AAB AAC\n" //
+                + "AAA 2 aaax aaay\n" //
+                + "AAB 2 aabx aaby\n" //
+                + "AAC 2 aacx aacy\n" //
+        ;
+
+        String output = "Case #1: 4\n";
+        assertEquals(output, mBowlSolver.solve(input));
+
+    }
+
+    @Test
+    public void constrainedByIngredientMixing() {
+        String input = "1\n" // comment
+                + recipes(9) //
+                + "A 3 AA AB\n" //
+                + "AA 3 AAA AAB AAC\n" //
+                + "AB 3 ABA ABB ABC\n" //
+                + "AAA 2 aaax aaay\n" //
+                + "AAB 2 aabx aaby\n" //
+                + "AAC 2 aacx aacy\n" //
+                + "ABA 2 abax abay\n" //
+                + "ABB 2 abbx abby\n" //
+                + "ABC 2 abcx abcy\n" //
+        ;
+
+        String output = "Case #1: 5\n";
+        assertEquals(output, mBowlSolver.solve(input));
+
+    }
+
+    private String recipes(final int n) {
+        return n + "\n";
     }
 }
--- src/BowlSolver.java
+++ src/BowlSolver.java
@@ -1,29 +1,57 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BowlSolver {
+
+    Map<String, List<String>> mIngredientsMap = new HashMap<String, List<String>>();
+
+    Map<String, Integer> mBowlsRequired = new HashMap<String, Integer>();
 
     public String solve(String input) {
         List<String> recipes = new ArrayList<String>(Arrays.asList(input.split("\n")));
         recipes.remove(1);
         recipes.remove(0);
-        int bowlsForRecipeList = -1;
         for (String recipe : recipes) {
-            int bowlsForRecipe = numberOfBowlsForRecipe(recipe);
-            bowlsForRecipeList = Math.max(bowlsForRecipeList, bowlsForRecipe);
+            final String name = parseName(recipe);
+            List<String> ingredients = parseIngredients(recipe);
+            mIngredientsMap.put(name, ingredients);
+        }
+        int bowlsForRecipeList = -1;
+        for (String recipe : mIngredientsMap.keySet()) {
+            bowlsForRecipeList = Math.max(bowlsForRecipeList, numberOfBowlsForRecipe(recipe));
         }
         return "Case #1: " + bowlsForRecipeList + "\n";
     }
 
+    private String parseName(String recipe) {
+        return recipe.split(" ")[0];
+    }
+
+    private List<String> parseIngredients(String recipe) {
+        List<String> ingredients = new ArrayList<String>(Arrays.asList(recipe.split(" ")));
+        ingredients.remove(1);
+        ingredients.remove(0);
+        return ingredients;
+    }
+
     private int numberOfBowlsForRecipe(String recipe) {
-        String[] words = recipe.split(" ");
-        int bowlsForRecipe = 0;
-        for (String word : words) {
+        // Check the cache
+        if (mBowlsRequired.containsKey(recipe)) {
+            return mBowlsRequired.get(recipe);
+        }
+
+        // Calculate if the value wasn't already cached
+        int bowlsForRecipe = 1; // We need one bowl to hold the resulting
+                                // mixture
+        for (String word : mIngredientsMap.get(recipe)) {
             if (Character.isUpperCase(word.charAt(0))) {
-                bowlsForRecipe++;
+                bowlsForRecipe += 1;
             }
         }
+        mBowlsRequired.put(recipe, bowlsForRecipe);
         return bowlsForRecipe;
     }
 
